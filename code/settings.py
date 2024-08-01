5f06722c53a946bd4b52f6ef34d864f5a6da6a21
ValentinDeCraene <valentin.de.craene@hotmail.fr>
2024-08-01 12:02:25 +0200

    ajout de l'UI, des méthodes de gestion des armes et des paramètres de l'UI

--- code/settings.py
+++ code/settings.py
@@ -3,3 +3,35 @@ WIDTH    = 1280
 HEIGTH   = 720
 FPS      = 60
 TILESIZE = 64
+
+# ui
+BAR_HEIGHT = 20
+HEALTH_BAR_WIDTH = 200
+ENERGY_BAR_WIDTH = 140
+ITEM_BOX_SIZE = 80
+UI_FONT = "../graphics/font/joystix.ttf"
+UI_FONT_SIZE = 18
+
+WATER_COLOR = '#71ddee'
+UI_BG_COLOR = '#222222'
+UI_BORDER_COLOR = '#111111'
+TEXT_COLOR = '#EEEEEE'
+
+# ui colors
+HEALTH_COLOR = 'red'
+ENERGY_COLOR = 'blue'
+UI_BORDER_COLOR_ACTIVE = 'gold'
+
+# upgrade menu
+TEXT_COLOR_SELECTED = '#111111'
+BAR_COLOR = '#EEEEEE'
+BAR_COLOR_SELECTED = '#111111'
+UPGRADE_BG_COLOR_SELECTED = '#EEEEEE'
+
+# weapons 
+weapon_data = {
+	'sword': {'cooldown': 100, 'damage': 15,'graphic':'../graphics/weapons/sword/full.png'},
+	'lance': {'cooldown': 400, 'damage': 30,'graphic':'../graphics/weapons/lance/full.png'},
+	'axe': {'cooldown': 300, 'damage': 20, 'graphic':'../graphics/weapons/axe/full.png'},
+	'rapier':{'cooldown': 50, 'damage': 8, 'graphic':'../graphics/weapons/rapier/full.png'},
+	'sai':{'cooldown': 80, 'damage': 10, 'graphic':'../graphics/weapons/sai/full.png'}}
--- code/support.py
+++ code/support.py
@@ -1,14 +1,6 @@
-from csv import reader
-import os
 import pygame
+import os
 
-def import_csv_layout(path):
-	terrain_map = []
-	with open(path) as level_map:
-		layout = reader(level_map,delimiter = ',')
-		for row in layout:
-			terrain_map.append(list(row))
-		return terrain_map
 def import_folder(path):
     surface_list = []
 
@@ -22,5 +14,37 @@ def import_folder(path):
                 except pygame.error as e:
                     print(f"Failed to load image at {full_path}: {e}")
 
+    if not surface_list:
+        raise FileNotFoundError(f"No images found in the folder: {path}")
+
     return surface_list
 
+from csv import reader
+from os import walk
+import pygame
+
+def import_csv_layout(path):
+	terrain_map = []
+	with open(path) as level_map:
+		layout = reader(level_map,delimiter = ',')
+		for row in layout:
+			terrain_map.append(list(row))
+		return terrain_map
+
+def import_folder(path):
+    surface_list = []
+
+    for _, __, img_files in os.walk(path):
+        for image in img_files:
+            if "IDENTIFIER" not in image:  # Exclure les fichiers contenant "IDENTIFIER"
+                full_path = os.path.join(path, image)
+                try:
+                    image_surf = pygame.image.load(full_path).convert_alpha()
+                    surface_list.append(image_surf)
+                except pygame.error as e:
+                    print(f"Failed to load image at {full_path}: {e}")
+
+    if not surface_list:
+        raise FileNotFoundError(f"No images found in the folder: {path}")
+
+    return surface_list
