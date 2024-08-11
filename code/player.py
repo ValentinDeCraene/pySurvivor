810003b204f83079a9053e70d8993e80bbcf1dc1
ValentinDeCraene <valentin.de.craene@hotmail.fr>
2024-08-11 18:02:34 +0200

    dev du système de level up et d'upgrade des attributs via le gain d'exp, dev de l'UI des stats et quelques corrections d'attributs

--- code/player.py
+++ code/player.py
@@ -192,6 +192,13 @@ class Player(Entity):
 		spell_damage = magic_data[self.magic]["strength"]
 		return base_damage + spell_damage
 
+	def get_value_by_index(self,index):
+		return list(self.stats.values())[index]
+
+	def get_cost_by_index(self,index):
+		return list(self.upgrade_cost.values())[index]
+
+
 	def energy_recovery(self):
 		if self.energy < self.stats["energy"]:
 			self.energy += 0.01 * self.stats["magic"]
@@ -203,5 +210,5 @@ class Player(Entity):
 		self.cooldowns()
 		self.get_status()
 		self.animate()
-		self.move(self.speed)
+		self.move(self.stats['speed'])
 		self.energy_recovery()
